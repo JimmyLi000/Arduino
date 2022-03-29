38a80bb5ab2e2dd0bbb77b5f95b5ee53775bd1c6
JimmyLi000 <102612222+JimmyLi000@users.noreply.github.com>
2022-03-30 00:52:34 +0800

    Add files via upload

---
+++ test_led.ino
@@ -0,0 +1,126 @@
+
+
+void setup() {
+
+pinMode(1, OUTPUT);//PIN繳輸出
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+
+}
+
+void loop() { 
+
+digitalWrite(1, HIGH); 
+delay(1000); 
+digitalWrite(1, LOW);
+delay(1000); 
+
+digitalWrite(2, HIGH); 
+delay(1000); 
+digitalWrite(2, LOW);
+delay(1000); 
+
+digitalWrite(3, HIGH); 
+delay(1000); 
+digitalWrite(3, LOW);
+delay(1000); 
+
+digitalWrite(4, HIGH); 
+delay(1000); 
+digitalWrite(4, LOW);
+delay(1000); 
+
+digitalWrite(5, HIGH); 
+delay(1000); 
+digitalWrite(5, LOW);
+delay(1000); 
+
+digitalWrite(6, HIGH); 
+delay(1000); 
+digitalWrite(6, LOW);
+delay(1000); 
+
+digitalWrite(7, HIGH); 
+delay(1000); 
+digitalWrite(7, LOW);
+delay(1000); 
+
+digitalWrite(1, HIGH); 
+delay(1000); 
+digitalWrite(1, LOW);
+delay(1000);
+
+digitalWrite(3, HIGH); 
+delay(1000); 
+digitalWrite(3, LOW);
+delay(1000); 
+
+digitalWrite(5, HIGH); 
+delay(1000); 
+digitalWrite(5, LOW);
+delay(1000);
+
+digitalWrite(7, HIGH); 
+delay(1000); 
+digitalWrite(7, LOW);
+delay(1000); 
+
+digitalWrite(2, HIGH); 
+delay(1000); 
+digitalWrite(2, LOW);
+delay(1000);
+
+digitalWrite(4, HIGH); 
+delay(1000); 
+digitalWrite(4, LOW);
+delay(1000); 
+
+digitalWrite(6, HIGH); 
+delay(1000); 
+digitalWrite(6, LOW);
+delay(1000); 
+
+digitalWrite(1, HIGH);
+digitalWrite(3, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(7, HIGH);
+delay(1000);
+digitalWrite(1, LOW);
+digitalWrite(3, LOW);
+digitalWrite(5, LOW);
+digitalWrite(7, LOW);
+delay(1000);
+
+digitalWrite(2, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(6, HIGH);
+delay(1000);
+digitalWrite(2, LOW);
+digitalWrite(4, LOW);
+digitalWrite(6, LOW);
+delay(1000);
+
+digitalWrite(1, HIGH);
+digitalWrite(2, HIGH);
+digitalWrite(3, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(7, HIGH);
+delay(1000);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+delay(1000);
+}
+
+
+
