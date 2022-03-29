082a04d19400764002f78c4e1efa5eeb6d1b762d
JimmyLi000 <102612222+JimmyLi000@users.noreply.github.com>
2022-03-30 00:24:06 +0800

    Add files via upload

---
+++ LED.ino
@@ -0,0 +1,36 @@
+#include<SoftwareSerial.h>    
+#include<Wire.h>             
+SoftwareSerial I2CBT(10,11);  //TX=PIN10  RX=PIN11
+
+
+void setup() {
+  Serial.begin(9600);  
+  I2CBT.begin(9600); 
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+   byte cmmd[20];
+    int insize;
+    while(1){
+      if((insize=(I2CBT.available())) >0){ //判斷是否有訊息傳入訊息
+        Serial.print("input size =");
+        Serial.println(insize);
+        for(int i=0; i<insize; i++){
+          Serial.print(cmmd[i]=char(I2CBT.read())); //顯示訊息
+          Serial.print(" ");
+        }
+      }
+    }
+  switch (inByte) {
+  case 'a':
+  digitalWrite(7,HIGH);
+  
+  break;
+
+  case 'b':
+  digitalWrite(7,LOW);
+  
+  break;
+  }
+}
